1228605d1b7d42e9a56007eebb084e7c3597526a
p <p>
2022-03-20 22:24:00 -0700

    periodic commit

--- tmux.sh
+++
@@ -1,8 +0,0 @@
-apt-get -y install libncurses5-dev libncursesw5-dev libevent-dev automake
-
-git clone https://github.com/tmux/tmux.git /tmp/tmux
-cd /tmp/tmux
-./autogen.sh
-./configure
-make -j $(nproc) && make install
-
